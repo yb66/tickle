38329d32666c9feb77d7d40000f91966406fe415
Jesse Aldridge <JesseAldridge@gmail.com>
2011-06-15 04:47:20 -0500

    Add timezone notes to example.rb

--- examples.rb
+++ examples.rb
@@ -1,7 +1,25 @@
 
 require File.join(File.dirname(__FILE__), 'lib', 'tickle')
 
-['May 30', '6:30 PM', 'tomorrow at 6:00', 'June 18, 2011',].each {|s|
-    print "s: ", s, " ", Tickle.parse(s)
-    puts
+=begin
+
+Time is ignored if there's also a date, unless the date is 'tomorrow'(?)
+
+Tickle creates times in the servers local time zone.
+To go from server time to user's local time...
+  user_time = server_time + (user_time_offset - server_time_offset)
+  eg (5:00PM Central) = (6:00PM Eastern + (-6 - -5))
+
+=end
+
+
+server_offset = Time.now.utc_offset / 60 / 60
+
+['May 30th at midnight', '6:30 PM', 
+ 'tomorrow at 6:00', 'June 18, 2011', 'Christmas'].each {|s|
+  time = Tickle.parse(s)[:next]
+  print s, " --> server date: ", time
+  puts
+  print s, " --> utc date: ", time - Time.now.utc_offset
+  puts
 }
