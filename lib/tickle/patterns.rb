860eb0ab17da27fbdff364ab98f931cc7c764d64
Iain Barnett <iainspeed@gmail.com>
2014-11-21 02:31:45 +0000

    Moved patterns into their own file to be organised, and added specs.

---
+++ lib/tickle/patterns.rb
@@ -0,0 +1,106 @@
+module Tickle
+
+  # A place to keep all the regular expressions.
+  module Patterns
+  
+    PLURAL_OR_PRESENT_PARTICIPLE = /
+        s
+          |
+        ing
+    /x
+
+    ON_THE = /
+      \bon\b
+      (?:
+        \s+
+        the
+      )?
+    /x
+
+    END_OR_UNTIL  = /
+      (?:
+        \bend
+        (?: #{PLURAL_OR_PRESENT_PARTICIPLE} )?
+        (?:
+          \s+
+          #{ON_THE}
+        )?
+      )
+        |
+      (:?
+        until
+        (?:
+          \s+
+          \bthe\b
+        )?
+      )
+    /x
+
+    SET_IDENTIFIER = /
+      every
+        |
+      each
+        |
+      #{ON_THE}
+        |
+      repeat
+    /x
+
+    START = /
+      start
+      (?: #{PLURAL_OR_PRESENT_PARTICIPLE} )?
+    /x
+
+    START_EVERY_REGEX = /^
+      (?:
+        #{START}
+      )
+      \s+
+      (?<start>.*?)
+      (?:
+        \s+
+        (?: #{SET_IDENTIFIER} )
+      )?
+      \s+
+      (?<event>.*)
+    /ix
+
+
+    EVERY_START_REGEX = /^
+      (?: #{SET_IDENTIFIER} )
+      \s+
+      (?<event>.*)
+      (?:
+        \s+
+        #{START}
+        (?:
+          \s+
+          #{ON_THE}
+        )?
+      )
+      \s+
+      (?<start>.*)
+    /ix
+
+    START_ENDING_REGEX = /^
+      #{START}
+      \s+
+      (?<start>.*)
+      (?:
+        \s+
+        #{END_OR_UNTIL}
+      )
+      \s+
+      (?<finish>.*)
+    /ix
+
+    PROCESS_FOR_ENDING = /^
+      (?<target>.*)
+      \s+
+      (?: #{END_OR_UNTIL})
+      \s+
+      (?<ending>.*)
+    /ix
+
+  end
+end
