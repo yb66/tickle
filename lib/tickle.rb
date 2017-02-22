1f053a343659a1c45b001ff253d5fdc7d3db8faf
Bjorn Skarner <bjorn@def-end.se>
2017-02-22 14:33:51 +0100

    Use a functioning date and time parsing to not burn up on odd months.
    When the date range passes outside of the current months days Tickle
    blows up. To prevent this from happening we use the built in methods to
    travel up and down in time.

--- lib/tickle.rb
+++ lib/tickle.rb
@@ -48,20 +48,20 @@ class Date #:nodoc:
     amount ||= 1
     case attr
     when :day then
-      Date.civil(self.year, self.month, self.day + amount)
+      Date.civil(self.year, self.month, self.day).next_day(amount)
     when :wday then
       amount = Date::ABBR_DAYNAMES.index(amount) if amount.is_a?(String)
       raise Exception, "specified day of week invalid.  Use #{Date::ABBR_DAYNAMES}" unless amount
       diff = (amount > self.wday) ? (amount - self.wday) : (7 - (self.wday - amount))
-      Date.civil(self.year, self.month, self.day + diff)
+      Date.civil(self.year, self.month, self.day).next_day(diff)
     when :week then
-      Date.civil(self.year, self.month, self.day + (7*amount))
+      Date.civil(self.year, self.month, self.day).next_day(7*amount)
     when :month then
-      Date.civil(self.year, self.month+amount, self.day)
+      Date.civil(self.year, self.month, self.day).next_month(amount)
     when :year then
-      Date.civil(self.year + amount, self.month, self.day)
+      Date.civil(self.year, self.month, self.day).next_year(amount)
     else
-            raise Exception, "type \"#{attr}\" not supported."
+      raise Exception, "type \"#{attr}\" not supported."
     end
   end
 end
@@ -71,27 +71,27 @@ class Time #:nodoc:
     amount ||= 1
     case attr
     when :sec then
-      Time.local(self.year, self.month, self.day, self.hour, self.min, self.sec + amount)
+      Time.local(self.year, self.month, self.day, self.hour, self.min, self.sec) + amount
     when :min then
-      Time.local(self.year, self.month, self.day, self.hour, self.min + amount, self.sec)
+      Time.local(self.year, self.month, self.day, self.hour, self.min, self.sec) + (amount * 60)
     when :hour then
-      Time.local(self.year, self.month, self.day, self.hour + amount, self.min, self.sec)
+      Time.local(self.year, self.month, self.day, self.hour, self.min, self.sec) + (amount * 60 * 60)
     when :day then
-      Time.local(self.year, self.month, self.day + amount, self.hour, self.min, self.sec)
+      Time.local(self.year, self.month, self.day, self.hour, self.min, self.sec) + (amount * 60 * 60 * 24)
     when :wday then
       amount = Time::RFC2822_DAY_NAME.index(amount) if amount.is_a?(String)
       raise Exception, "specified day of week invalid.  Use #{Time::RFC2822_DAY_NAME}" unless amount
       diff = (amount > self.wday) ? (amount - self.wday) : (7 - (self.wday - amount))
-      Time.local(self.year, self.month, self.day + diff, self.hour, self.min, self.sec)
+      DateTime.civil(self.year, self.month, self.day, self.hour, self.min, self.sec, self.zone).next_day(diff)
     when :week then
-      Time.local(self.year, self.month, self.day + (amount * 7), self.hour, self.min, self.sec)
+      DateTime.civil(self.year, self.month, self.day, self.hour, self.min, self.sec, self.zone).next_day(amount * 7)
     when :month then
-      Time.local(self.year, self.month + amount, self.day, self.hour, self.min, self.sec)
+      DateTime.civil(self.year, self.month, self.day, self.hour, self.min, self.sec, self.zone).next_month(amount)
     when :year then
-      Time.local(self.year + amount, self.month, self.day, self.hour, self.min, self.sec)
+      DateTime.civil(self.year, self.month, self.day, self.hour, self.min, self.sec, self.zone).next_year(amount)
     else
       raise Exception, "type \"#{attr}\" not supported."
-    end
+    end.to_time.localtime
   end
 end
 
